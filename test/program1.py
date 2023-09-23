4043b207ea6658f6a254d966ac238507841623cd
catalunha <catalunha.mj@gmail.com>
2023-09-22 21:15:07 -0300

    r3v

--- test/program1.py
+++ test/program1.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.models import User
-from api.models import ResetPasswordNumberModel
+# from django.contrib.auth.models import User
+
+from account.models import ResetPasswordNumberModel
 
 list = ResetPasswordNumberModel.objects.all()
 for item in list:
     print(item)
 
 
-user = User.objects.get(username="a1@gmail.com")
-print(user.id)
+# user = User.objects.get(username="a1@gmail.com")
+# print(user.id)
